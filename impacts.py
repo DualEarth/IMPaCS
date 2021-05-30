dbe3f1d1401f336dec8bd92c8a58bd50e91c1004
jmframe <jmframe@crimson.ua.edu>
2021-05-29 19:02:54 -0500

    Running some ensembles, plotting esemble results

--- impacts.py
+++ impacts.py
@@ -8,6 +8,7 @@ from ease_grid import EASE2_grid
 import matplotlib
 matplotlib.use('Agg') # Must be before importing matplotlib.pyplot or pylab!
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
 # approximate radius of earth in km
 def distance(lat1,lat2,lon1,lon2):
@@ -28,14 +29,11 @@ def distance(lat1,lat2,lon1,lon2):
 
     return(distance) #km
 
-
-
-
-
 class IMPAaCS:
 
     """
-    Last update: May 28th 2021 @ 7:19PM Central Time, with Jordan 
+    Update: May 29th 2021 @ 2:30PM Central Time, Search only the subset of grids for impact, else skip
+    Update: May 28th 2021 @ 12:19PM Central Time, with Jordan to get ensembles running
     
     Dynamic geospatial model of IMPaCS, 
     using the size-frequency distribution of impacts scaled from the lunar surface, 
@@ -43,7 +41,7 @@ class IMPAaCS:
     during the Hadean to determine how rapidly it evolved.
     """
     
-    impact_test_id = str(round(-87.5726,4))+' '+str(round(33.2921,4))
+    impact_test_id = str(round(0.14122179,4))+' '+str(round(0.18672199,4))
     
     def __init__(self, egrid, 
                  verbose=False,
@@ -57,7 +55,7 @@ class IMPAaCS:
                  z_discretized_km = int(2),
                  proportion_melt_from_impact = 1/3,
                  sim_time=0,
-                 x_lims = [-180, 180], y_lims = [-45, 45]):
+                 lon_lims = [-180, 180], lat_lims = [-45, 45]):
         self.egrid = egrid
         self.verbose=verbose
         self.ensemble=ensemble
@@ -83,20 +81,29 @@ class IMPAaCS:
         self.impacted_grid_cells = []
         self.impactors_at_test_cell = [0]
         self.test_time = [0]
-        
-        self.state_prep()
-        
+                
         self.sample_percents = {}
-        self.x_lims=x_lims
-        self.y_lims=y_lims
+        self.lon_lims=lon_lims
+        self.lat_lims=lat_lims
+        self.lon_subset=[]
+        self.lat_subset=[]
+        
+        # Set up the grids so we don't search the whole planet for every impact
+        self.get_subset_of_grids()
+        self.n_x = len(self.lon_subset)
+        self.n_y = len(self.lat_subset)
+        
+        # Finally we set up the state data
+        self.state_prep()
         
     #--------------------------------------------------------------------------------------------------
     def update(self, impact_loc, impactor_diameter, sim_time=0):
         self.sim_time = sim_time
         self.impact_dimensions(impactor_diameter)
         self.find_the_grid(impact_loc)
-        self.get_average_target(impactor_diameter)
-        self.loop_impact_grid(impactor_diameter)
+        if len(self.impacted_grid_cells) > 0:
+            self.get_average_target(impactor_diameter)
+            self.loop_impact_grid(impactor_diameter)
         
     #--------------------------------------------------------------------------------------------------
     #---- THIS IS THE MAIN CODE -------------------- THIS IS THE MAIN CODE ----------------------------
@@ -144,11 +151,11 @@ class IMPAaCS:
     #--------------------------------------------------------------------------------------------------    
     def state_prep(self):
         total_layers = int(self.max_depth_of_impact_melt / self.z_discretized_km)
-        for ilon in self.egrid.londim:
-            for ilat in self.egrid.latdim:
+        for ilon in self.lon_subset:
+            for ilat in self.lat_subset:
                 grid_cell_id = str(round(ilon,4))+' '+str(round(ilat,4))
                 self.grid_cell_state[grid_cell_id] = np.ones(total_layers) * self.primitive_initial_state
-    
+        
     #--------------------------------------------------------------------------------------------------    
     def get_average_target(self, impactor_diameter):
         average_target = 0
@@ -165,24 +172,30 @@ class IMPAaCS:
     def find_the_grid(self, impact_loc):
         self.impacted_grid_cells = [] # first reset the impacted grid cells, then fill them up
         Dmin=10000000
-        for ilon in self.egrid.londim:
-            for ilat in self.egrid.latdim:
+        for ilon in self.lon_subset:
+            for ilat in self.lat_subset:
                 D = distance(impact_loc[0],ilat,impact_loc[1],ilon)
                 if D < Dmin:
                     Dmin = D
                 if D <= self.crator_radius:
                     self.impacted_grid_cells.append([ilon, ilat])
         if len(self.impacted_grid_cells) < 1:
-            if self.verbose:
-                print("Warning. There are no grids impacted!")
-                print('Dmin', Dmin, 'crator radius', self.crator_radius, 'impact location', impact_loc)
-            for ilon in self.egrid.londim:
-                for ilat in self.egrid.latdim:
-                    D = distance(impact_loc[0],ilat,impact_loc[1],ilon)
-                    if D == Dmin:
-                        self.impacted_grid_cells.append([ilon, ilat])
-                        if self.verbose:
-                            print('impacting grid cell', [ilon, ilat])
+            
+            # If the crator didn't impact any grids in the subsample, 
+            # Check to see if the min distance is smaller than the length of a grid.
+            # If it is, we can assign it to the closest grid.
+            # If not, then just ignore it.
+            if Dmin < 30:
+                if self.verbose:
+                    print("Warning. There are no grids impacted!")
+                    print('Dmin', Dmin, 'crator radius', self.crator_radius, 'impact location', impact_loc)
+                for ilon in self.lon_subset:
+                    for ilat in self.lat_subset:
+                        D = distance(impact_loc[0],ilat,impact_loc[1],ilon)
+                        if D == Dmin:
+                            self.impacted_grid_cells.append([ilon, ilat])
+                            if self.verbose:
+                                print('impacting grid cell', [ilon, ilat])
 
     #--------------------------------------------------------------------------------------------------    
     def loop_impact_grid(self, impactor_diameter):
@@ -211,7 +224,7 @@ class IMPAaCS:
             self.top_layer_at_test_cell.append(self.grid_cell_state[self.impact_test_id][0])
 
     #--------------------------------------------------------------------------------------------------
-    def re_bin_sio2(self, temp_state, s_min=34, s_max=70, ds=2):
+    def re_bin_sio2(self, temp_state, s_min=34, s_max=70, ds=1):
         """
             Functionto place the mean SiO2 into the proper bin for distribution.
         """
@@ -224,9 +237,7 @@ class IMPAaCS:
                 continue
 
     # ---------------------------------------------------------------------------------------------
-    def plot_map_and_bar(self, save_figure=False, plot_figure=False, fig_path='./',
-                         plot_x_lims = [-180, 180],
-                         plot_y_lims = [-45, 45]):
+    def plot_map_and_bar(self, save_figure=False, plot_figure=False, fig_path='./'):
         
         """
             Function for plotting 2D map of SiO2 States.
@@ -242,21 +253,21 @@ class IMPAaCS:
 
         z = np.zeros([self.n_x, self.n_y])
         bar_list = []
-        for i, ilon in enumerate(self.egrid.londim):
-            for j, ilat in enumerate(self.egrid.latdim):
+        for i, ilon in enumerate(self.lon_subset):
+            for j, ilat in enumerate(self.lat_subset):
                 grid_cell = str(round(ilon,4))+' '+str(round(ilat,4))
                 temp_state = np.mean(self.grid_cell_state[grid_cell][0:2])
                 temp_state = self.re_bin_sio2(temp_state)
                 z[i, j] = temp_state
         
-        X, Y = np.meshgrid(self.egrid.londim, self.egrid.latdim)
+        X, Y = np.meshgrid(self.lon_subset, self.lat_subset)
         
-        fig = plt.figure(figsize=(15, 5))
+        fig = plt.figure(figsize=(12, 7))
 
         grid = plt.GridSpec(1, 7, wspace = .1, hspace = .1)
         plt.subplots_adjust(wspace= 0.1, hspace= 0.1)
 
-        plt.subplot(grid[0, :6])
+        plt.subplot(grid[0, :5])
         
         levels = np.arange(34, 70, 2)
         cmap = cm.jet
@@ -266,11 +277,11 @@ class IMPAaCS:
         plt.title('Surface SiO2 content at {}myr'.format(int(self.sim_time/1000000)))
         plt.xlabel('longitude')
         plt.ylabel('latitude')
-        plt.xlim(plot_x_lims)
-        plt.ylim(plot_y_lims)
-        plt.xticks(np.arange(plot_x_lims[0], plot_x_lims[1], 10))
+        plt.xlim(self.lon_lims)
+        plt.ylim(self.lat_lims)
+        plt.xticks(np.arange(self.lon_lims[0], self.lat_lims[1], 10))
         
-        plt.subplot(grid[0, 6])
+        plt.subplot(grid[0, 5:])
         plt.bar(list(self.sample_percents.keys()), list(self.sample_percents.values()), width=1.2)
         plt.xlim([35,70])
         plt.ylim([0,40])
@@ -285,7 +296,7 @@ class IMPAaCS:
         plt.close()
         
     # ---------------------------------------------------------------------------------------------
-    def do_sample_percents(self, x_lims = [-180, 180], y_lims = [-45, 45], n_layers=2):
+    def do_sample_percents(self, n_layers=2):
         
         """
             Function Summarizing and saving SiO2 percentages in a sample region.
@@ -297,32 +308,34 @@ class IMPAaCS:
         
         z = np.zeros([self.n_x, self.n_y])
         bar_list = []
-        for i, ilon in enumerate(self.egrid.londim):
-            for j, ilat in enumerate(self.egrid.latdim):
+        for i, ilon in enumerate(self.lon_subset):
+            for j, ilat in enumerate(self.lat_subset):
                 grid_cell = str(round(ilon,4))+' '+str(round(ilat,4))
                 temp_state = np.mean(self.grid_cell_state[grid_cell][0:2])
                 temp_state = self.re_bin_sio2(temp_state)
                 z[i, j] = temp_state
                 
-                # Don't analyze anything outside the plot bounds
-                if float(grid_cell.split(" ")[1]) < y_lims[0]:
-                    continue
-                elif float(grid_cell.split(" ")[1]) > y_lims[1]:
-                    continue
-                elif float(grid_cell.split(" ")[0]) < x_lims[0]:
-                    continue
-                elif float(grid_cell.split(" ")[0]) > x_lims[1]:
-                    continue
-                else:
-                    mean_sio2 = np.mean(self.grid_cell_state[grid_cell][0:2])
-                    if not np.isnan(mean_sio2):
-                        bar_list.append(self.re_bin_sio2(mean_sio2))
+                mean_sio2 = np.mean(self.grid_cell_state[grid_cell][0:2])
+                if not np.isnan(mean_sio2):
+                    bar_list.append(self.re_bin_sio2(mean_sio2))
         
         bar_list = [x for x in bar_list if x != None]
         
-        X, Y = np.meshgrid(self.egrid.londim, self.egrid.latdim)
         bar_data = {}
         for u in np.unique(bar_list):
             bar_data[u] = 100*bar_list.count(u)/len(bar_list)
 
         self.sample_percents = bar_data
+        
+    # ---------------------------------------------------------------------------------------------
+    def get_subset_of_grids(self):
+        for ilon in self.egrid.londim:
+            for ilat in self.egrid.latdim:
+                if ilat > self.lat_lims[0] and ilat < self.lat_lims[1]:
+                    if ilon > self.lon_lims[0] and ilon < self.lon_lims[1]:
+                        if ilat not in self.lat_subset:
+                            self.lat_subset.append(ilat)
+                        if ilon not in self.lon_subset:
+                            self.lon_subset.append(ilon)
+
+
